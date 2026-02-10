6df1af28e77f7423af9d6960e03f7de1c445324f
Moondip Konwar <moondipkonwar9@gmail.com>
2026-02-10 21:03:48 +0530

    Title + Favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,10 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>JSON Forms</title>
+</svelte:head>
 
 <!-- Navbar -->
 <nav
