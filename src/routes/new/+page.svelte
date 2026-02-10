ff26ab410163f54ee30f7677c6c9629dff601bda
Moondip Konwar <moondipkonwar9@gmail.com>
2026-02-10 14:58:09 +0530

    Redesigned new page

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,59 +1,84 @@
-<div class="px-8 py-4 flex justify-center items-center">
-  <!-- Card -->
-  <section class="prose p-8 w-full">
-    <!-- Title -->
-    <h2 class="text-center">Create New Form</h2>
+<script>
+  import { Plus } from "lucide-svelte";
+</script>
 
-    <!-- Form -->
-    <form action="" on:submit|preventDefault class="flex flex-col gap-6 mt-8">
-      <!-- Form Fields -->
-      <div class="flex flex-col gap-8">
-        <!-- Form Name -->
-        <div class="flex flex-col gap-1">
-          <label for="form-name" class="font-medium">Form Name *</label>
-          <input
-            type="text"
-            required
-            id="form-name"
-            name="form-name"
-            placeholder="Enter form name"
-            class="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          />
-        </div>
+<form class="px-8 py-4 flex flex-row gap-8">
+  <!-- Form Details -->
+  <section class="prose">
+    <h3>Form Details</h3>
+    <!-- Form Name -->
+    <div class="form-control">
+      <label for="form-name" class="label">Form Name *</label>
+      <input
+        id="form-name"
+        required
+        type="text"
+        placeholder="Enter form name"
+      />
+    </div>
 
-        <!-- Form Description -->
-        <div class="flex flex-col gap-1">
-          <label for="form-description" class="font-medium"
-            >Form Description</label
-          >
-          <textarea
-            id="form-description"
-            name="form-description"
-            placeholder="Enter form description"
-            class="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          />
-        </div>
-      </div>
+    <!-- Form Description -->
+    <div class="form-control">
+      <label for="form-description" class="label">Form Description</label>
+      <textarea
+        id="form-description"
+        placeholder="Enter form description"
+        rows="4"
+      ></textarea>
+    </div>
 
-      <!-- Author Name -->
-      <div class="flex flex-col gap-1">
-        <label for="author-name" class="font-medium">Author Name</label>
-        <input
-          type="text"
-          id="author-name"
-          name="author-name"
-          placeholder="Enter author name"
-          class="border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-        />
-      </div>
-
-      <!-- Submit Button  -->
-      <button
-        type="submit"
-        class="mt-6 bg-black/95 text-white font-medium rounded-lg px-4 py-2 hover:bg-black/80 transition-colors duration-200"
-      >
-        Create Form
-      </button>
-    </form>
+    <!-- Author Name -->
+    <div class="form-control">
+      <label for="author-name" class="label">Author Name</label>
+      <input id="author-name" type="text" placeholder="Enter author name" />
+    </div>
   </section>
-</div>
+
+  <!-- Form Content -->
+  <section class="prose">
+    <h3>Form Content</h3>
+    <!-- Add Field -->
+    <div class="flex flex-row gap-4">
+      <!-- Label -->
+      <input type="text" placeholder="Field Label" class="flex-1" />
+
+      <!-- Field Types -->
+      <select class="bg-white border border-gray-300 rounded-md p-2">
+        <option value="text">Text</option>
+        <option value="number">Number</option>
+      </select>
+
+      <!-- Button -->
+      <button
+        type="button"
+        class="cursor-pointer bg-black text-white rounded-md p-2 flex items-center gap-2"
+        on:click={() => alert("Add Field")}
+      >
+        <Plus size={16} /> Add Field
+      </button>
+    </div>
+  </section>
+</form>
+
+<style>
+  form > section {
+    flex: 1;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    height: fit-content;
+  }
+
+  .form-control {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+  input,
+  textarea {
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+</style>
