31e48dbe10e0d366047865832ade3b8f4a8f0798
Moondip Konwar <moondipkonwar9@gmail.com>
2026-02-10 21:07:05 +0530

    Config edits:

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "404.html",
+    }),
+
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/json-form-builder" : "",
+    },
+  },
+};
 
 export default config;
